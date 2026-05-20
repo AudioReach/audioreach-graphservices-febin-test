4e784d3be415162c36c9b91d3540310104297e4a
ffrancis123 <ffrancis@qti.qualcomm.com>
2026-05-20 11:34:08 +0530

    Update config.sh
    Signed-off-by: ffrancis123 <ffrancis@qti.qualcomm.com>

--- ci/config.sh
+++ ci/config.sh
@@ -4,6 +4,7 @@
 # Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
 # This file is part of the Audioreach CI configuration.
 # It defines the repositories and patches used in the CI process.
+
 REPOS=(
   "meta-ar|https://github.com/Audioreach/meta-ar.git"
 )
